6d54fa5814ab9ec70defb82650ee85a033e77e2f
nulldayexploits <nulldayexploits1117@gmail.com>
2021-06-06 20:30:45 +0800

    penambahan keamanan generate init

--- generate_init.php
+++ generate_init.php
@@ -4,8 +4,8 @@ function exception($value){
 
 	if(
 	
-		$value != ".git" &&      // file yang di kecualikan
-		$value != "data_upload"  // file yang di kecualikan
+		$value != ".git" && 
+		$value != "data_upload"
 
 	  ){ 
 		return true; 
@@ -46,27 +46,64 @@ function getDirContents($dir, &$results = array()) {
     return array_map('strip_special_chars',$results);
 }
 
-$scan  = getDirContents('../');
+?>
 
-$total = count($scan);
+<h3>Masukkan Username Dan Password</h3>
+<form method="post">
+	<input type="text" name="usr" placeholder="Username"><br>
+	<input type="password" name="pdd" placeholder="Password"><br>
+	<input type="submit" name="Submit" value="SUBMIT">
+</form>
+<?php
 
-$isi   = '<?php $init = array(';
+if(isset($_POST['Submit'])){
 
-$no = 1;
-foreach ($scan as $a) {
+	if($_POST['usr'] == "admin" && $_POST['pdd'] == "palaguna_cpanel_33"){
 
-	if($total==$no){
-	   $isi .= '"'.$a.'"';
+
+		$scan  = getDirContents('../');
+
+		$total = count($scan);
+
+		$isi   = '<?php $init = array(';
+
+		$no = 1;
+		foreach ($scan as $a) {
+
+			if($total==$no){
+			   $isi .= '"'.$a.'"';
+			}else{
+			   $isi .= '"'.$a.'", ';
+		    }
+			$no++;
+		}
+
+		$isi .= '); ?>';
+
+		$myfile   = fopen("init.php", "r") or die("Unable to open file!");
+		$old_init = fread($myfile,filesize("init.php"));
+		fclose($myfile);
+
+		$file1=fopen("backup_init/init.php", "a+") or die("File tidak ada");     
+		fwrite($file1,$old_init);
+		fclose($file1);
+
+
+		$file2=fopen("init.php", "w+") or die("File tidak ada");     
+		fwrite($file2,$isi);
+		fclose($file2);
+
+		echo 'Generate Init Success!!';
+
 	}else{
-	   $isi .= '"'.$a.'", ';
-    }
-	$no++;
+
+		echo 'Username atau Password anda salah';
+	
+	}
+
+}else{
+
+	echo 'Tekan Submit';
 }
-
-$isi .= '); ?>';
-
-$file=fopen("init.php", "w+") or die("File tidak ada");     
-fwrite($file,$isi);
-fclose($file);
 
 ?>
